1ea24fef2eb28d40b547dd59419aee4d46fb977e
wilhelmmichto <michto15897@gmail.com>
2022-04-11 22:22:41 +0530

    Updates

---
+++ precache-manifest.c09f969004f3616d5b6fc8a09c85a8a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b760ca2393e481e3f32",
+    "url": "/eth-todo-list-react/static/css/main.3ae4307e.chunk.css"
+  },
+  {
+    "revision": "8b760ca2393e481e3f32",
+    "url": "/eth-todo-list-react/static/js/main.8b760ca2.chunk.js"
+  },
+  {
+    "revision": "6aed96abdb92daf947d7",
+    "url": "/eth-todo-list-react/static/css/1.ddde046b.chunk.css"
+  },
+  {
+    "revision": "6aed96abdb92daf947d7",
+    "url": "/eth-todo-list-react/static/js/1.6aed96ab.chunk.js"
+  },
+  {
+    "revision": "3107806485e1415952d1",
+    "url": "/eth-todo-list-react/static/js/runtime~main.31078064.js"
+  },
+  {
+    "revision": "ac8ecfecf168ed0e88cf930c6e0c6ee9",
+    "url": "/eth-todo-list-react/index.html"
+  }
+];
