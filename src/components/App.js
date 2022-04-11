bb6f4db8514a2f5724c91fbbbbe97f4dd4aa78ae
wilhelmmichto <michto15897@gmail.com>
2022-04-11 18:23:15 +0530

    2. Solidity Backened Setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Web3 from "web3";
-
+import TodoList from '../truffle_abis/TodoList.json'
 
 class App extends Component {
   async UNSAFE_componentWillMount() {
@@ -24,22 +24,93 @@ class App extends Component {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
-    const network = await web3.eth.net.getNetworkType();
-    console.log(accounts);
-    console.log(network);
+    const networkId = await web3.eth.net.getId();
+   
+    //Load TODO Contract
+    const todoData = TodoList.networks[networkId];
+    if (todoData) {
+      const todoList = new web3.eth.Contract(TodoList.abi, todoData.address);
+      this.setState({ todoList });
+      let taskCount = await todoList.methods.taskCount().call()
+      this.setState({taskCount})
+      console.log(taskCount);
+      for (var i = 1; i <= taskCount; i++) {
+        const task = await todoList.methods.tasks(i).call()
+        this.setState({
+          tasks: [...this.state.tasks, task]
+        })
+      }
+    } else {
+      window.alert("Todo List not deployed to the network");
+    }
+    
   }
 
-  constructor(props){
-    super(props)
-    this.state = {account: ''}
-  }
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      taskCount: 0,
+      tasks:[],
+      
+    }
+
+    
+  };
+
+
+
+
+  
+
+  
 
   render() {
-    return <div>
-      <h1>hello world</h1>
-      <p>Your Account: {this.state.account} </p>
-    </div>;
-  }
+    return (
+      <div>
+          <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+          <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+        </li>
+      </ul>
+    </nav>
+    <div className="container-fluid">
+      <div className="row">
+        <main role="main" className="col-lg-12 d-flex justify-content-center">
+          <div id="loader" className="text-center">
+            <p className="text-center">Loading...</p>
+          </div>
+          <div id="content">
+            <form onSubmit="App.createTask(); return false;">
+              <input id="newTask" type="text" className="form-control" placeholder="Add task..." required/>
+              <input type="submit" hidden=""/>
+            </form>
+            <ul id="taskList" className="list-unstyled">
+              {this.state.tasks.map((task,key)=>{
+                return(
+                <div className="taskTemplate checkbox" key={key} >
+                <label>
+                  <input type="checkbox" />
+                  <span className="content">{task.content}</span>
+                </label>
+              </div>
+              )
+              })}
+              
+            </ul>
+            <ul id="completedTaskList" className="list-unstyled">
+            </ul>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+    );
+  };
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,72 @@
+export const TODO_LIST_ADDRESS='0x04aCf0d2D0040f6CBeA95B5EC01CD9e478d1947d'
+export const TODO_LIST_ABI=[
+  
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "taskCount",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "tasks",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "internalType": "string",
+            "name": "content",
+            "type": "string"
+          },
+          {
+            "internalType": "bool",
+            "name": "completed",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_content",
+            "type": "string"
+          }
+        ],
+        "name": "createTask",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+
+]
